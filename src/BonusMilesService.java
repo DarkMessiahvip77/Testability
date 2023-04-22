eb59a6f6390bfa5d2687c0785b010095022c8c36
“Ilya” <mitsukiv@yandex.ru>
2023-04-22 09:30:21 +0300

    add src/

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,5 @@
 public class BonusMilesService {
-    public int calculate (int price) {
+    public int calculate(int price) {
         int result;
         result = price / 20;
         return result;
